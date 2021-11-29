bba39838ffc5be421f7d002a3daa2b99684030d8
Arianna Bolzoni <Arianna.Bolzoni@icubed.it>
2021-11-29 15:57:01 +0100

    r vs i

--- PreAcademiA.Week3.RicorsioneVsIterazione/Esercizi.cs
+++ PreAcademiA.Week3.RicorsioneVsIterazione/Esercizi.cs
@@ -40,6 +40,8 @@ namespace PreAcademiA.Week3.RicorsioneVsIterazione
 
 
 
+
+
     // Scrivere una funzione che dato un importo di denaro iniziale,
     // un interesse annuo( 3% )  e un numero di anni permette di calcolare
     // l’importo di denaro accresciuto degli interessi dopo il numero di anni passati
@@ -52,5 +54,113 @@ namespace PreAcademiA.Week3.RicorsioneVsIterazione
     // Dopo 3 anni : 10609 + (10609 * 3 / 100) = 10609 + 318,27 = 10927,27
     class Esercizi
     {
+        internal static long FattorialeIterazione(int n)
+        {
+            long f = 1;
+            while (n > 0)
+                f *= n--;
+
+            return f;
+        }
+        internal static long FattorialeRicorsione(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * FattorialeRicorsione(n - 1);
+
+            //return n == 0 ? 1 : n * FattRicorsivo(n - 1);
+        }
+        // 0 1 2 3 4
+        // r a d a r
+        // p i p p o
+        internal static bool PalindromaIterazione(string s)
+        {
+            int count = s.Length;
+
+            for(int i = 0; i< (int)s.Length / 2; i++)
+            {
+                if(s[i] != s[--count])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // 0 1 2 3 4
+        // r a d a r
+        // X a d a X
+        // X X d X X 
+        // p i p p o
+        internal static bool PalindromaRicorsione(string s)
+        {
+            if (s.Length <= 1)
+                return true;
+
+            if (s[0] == s[s.Length - 1])
+                return PalindromaRicorsione(s.Substring(1, s.Length - 2));
+            else
+                return false;
+
+            //return s.Length <= 1 || (s[0] == s[s.Length - 1] &&
+            //    Palindroma(s.Substring(1, s.Length - 2)));
+        }
+
+        // 0 1 1 2 3 5 8 13 21 34 55 89 144
+        // 0 1 2 3 4 5 6 7  8  9  10 11 12
+        internal static int FibonacciIterazione(int n)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+
+            if (n == 0)
+                return n;
+            if (n == 1)
+                return n;
+
+            int somma = firstNumber + secondNumber;  // calcolo il primo valore della serie
+
+            for (int i = 2; i < n; i++)
+            {
+                firstNumber = secondNumber;
+                secondNumber = somma;
+                somma = firstNumber + secondNumber;
+            }
+
+            return somma;
+        }
+        internal static int FibonacciRicorsione(int n)
+        {
+            //if(n <= 1)
+            //{
+            //    return n;
+            //}
+            //else
+            //{
+            //    return FibonacciRicorsione(n - 2) + FibonacciRicorsione(n - 1);
+            //}
+
+            return n <= 1 ? n : FibonacciRicorsione(n - 2) + FibonacciRicorsione(n - 1);
+        }
+
+        internal static double InteressiIterazione(double capitale, int anni)
+        {
+            for (var i = 0; i < anni; i++)
+            {
+                capitale = capitale + (capitale * 3 / 100);
+            }
+            return capitale;
+        }
+        internal static double InteressiRicorsione(double capitale, int anni)
+        {
+            if (anni > 0)
+            {
+                return InteressiRicorsione(capitale + (capitale * 3 / 100), --anni);
+            }
+            else
+            {
+                return capitale;
+            }
+        }
     }
 }
--- PreAcademiA.Week3.RicorsioneVsIterazione/Program.cs
+++ PreAcademiA.Week3.RicorsioneVsIterazione/Program.cs
@@ -11,9 +11,36 @@ namespace PreAcademiA.Week3.RicorsioneVsIterazione
 
             /// Dato un numero a e una potenza b
             // stampare a alla b
-            
-            Eleva(3, 4);
-            Console.WriteLine(ElevaRicorsione(3, 4));
+
+            //Eleva(3, 4);
+            //Console.WriteLine(ElevaRicorsione(3, 4));
+
+            // Fattoriale
+            var risultato = Esercizi.FattorialeIterazione(5);
+            Console.WriteLine($"Il fattoriale calcolato con l'iterazione è {risultato}");
+            risultato = Esercizi.FattorialeRicorsione(5);
+            Console.WriteLine($"Il fattoriale calcolato con la ricorsione è {risultato}");
+
+            // Palindroma
+            //string s1 = "radar";
+            //Console.WriteLine($"Iteazione: la parola {s1} è palindroma? {Esercizi.PalindromaIterazione(s1)}");
+            //Console.WriteLine($"Ricorsione: la parola {s1} è palindroma? {Esercizi.PalindromaRicorsione(s1)}");
+            //s1 = "academy";
+            //Console.WriteLine($"Iteazione: la parola {s1} è palindroma? {Esercizi.PalindromaIterazione(s1)}");
+            //Console.WriteLine($"Ricorsione: la parola {s1} è palindroma? {Esercizi.PalindromaRicorsione(s1)}");
+
+            //Fibonacci
+            //int numero = 2;
+            //Console.WriteLine($"il numero in posizione {numero} della serie di Fibonacci è {Esercizi.FibonacciIterazione(numero)}");
+            //Console.WriteLine($"il numero in posizione {numero} della serie di Fibonacci è {Esercizi.FibonacciRicorsione(numero)}");
+
+            //Interessi
+            //double capitale = 10000;
+            //int anni = 3;
+            //Console.WriteLine($"Iterazione: con un capitale di {capitale} euro, dopo {anni} " +
+            //    $"anni si avranno {Esercizi.InteressiIterazione(capitale, anni)} euro");
+            //Console.WriteLine($"Ricorsione: con un capitale di {capitale} euro, dopo {anni} " +
+            //    $"anni si avranno {Esercizi.InteressiRicorsione(capitale, anni)} euro");
         }
 
         static void Eleva(int a, int b)
