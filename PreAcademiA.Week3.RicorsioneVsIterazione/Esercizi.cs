3cfea7d87a9038069519c3d3c01f867224bab754
Arianna Bolzoni <Arianna.Bolzoni@icubed.it>
2021-11-29 11:48:35 +0100

    ricorsione vs iterazione

---
+++ PreAcademiA.Week3.RicorsioneVsIterazione/Esercizi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreAcademiA.Week3.RicorsioneVsIterazione
+{
+    // Calcolare il fattoriale di un numero
+    // n! = n * (n - 1) * (n - 2) ..... * 1
+
+    //      1 se n == 0    
+    // n! = 
+    //      n * (n - 1)! altrimenti
+
+    // 5! = 5 * 4!
+    // 4! = 4 * 3!
+    // 3! = 3 * 2!
+    // 2! = 2 * 1!
+    // 1! = 1 * 0!
+    // 0! = 1
+
+
+
+    // una parola è palindroma... se ha al massimo 1 carattere
+    // oppure
+    // se la prima lettera è uguale all'ultima e
+    // il resto della parola è... palindroma
+    // "radar"
+
+    //[0] == [lenght -1]  -> x
+    //[1] == [x -1]   -> x
+
+
+
+    // restituisce l'ennesimo numero della serie di fibonacci
+    // 0 1 1 2 3 5 8 13 21 34 55 89 144
+    // 0 1 2 3 4 5 6 7  8  9  10 11 12
+
+
+
+
+    // Scrivere una funzione che dato un importo di denaro iniziale,
+    // un interesse annuo( 3% )  e un numero di anni permette di calcolare
+    // l’importo di denaro accresciuto degli interessi dopo il numero di anni passati
+
+    // Esempio
+    // Voglio vincolare 10000 euro per 3 anni con un interesse del 3%
+    //                                                         10000
+    // Dopo 1 anno : 10000 + (10000 * 3 / 100) = 10000 + 300 = 10300
+    // Dopo 2 anni : 10300 + (10300 * 3 / 100) = 10300 + 309 = 10609
+    // Dopo 3 anni : 10609 + (10609 * 3 / 100) = 10609 + 318,27 = 10927,27
+    class Esercizi
+    {
+    }
+}
---
+++ PreAcademiA.Week3.RicorsioneVsIterazione/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PreAcademiA.Week3.RicorsioneVsIterazione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Voler stampare tutti i numeri da n a 1
+            // StampaNumeri(5);
+
+            /// Dato un numero a e una potenza b
+            // stampare a alla b
+            
+            Eleva(3, 4);
+            Console.WriteLine(ElevaRicorsione(3, 4));
+        }
+
+        static void Eleva(int a, int b)
+        {
+            int potenza = 1;
+
+            for (int i = 0; i < b; i++)
+            {
+                potenza = potenza * a;
+            }
+            Console.WriteLine(potenza);
+        }
+        static int ElevaRicorsione(int a, int b)
+        {
+            if(b == 0)
+            {
+                a = 1;
+            }
+            else
+            {
+                a = a * ElevaRicorsione(a, b -1);
+            }
+
+            return a;
+        }
+
+
+        //5
+        static void StampaNumeri(int n)
+        {
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //while (n > 0)
+            //{
+            //    Console.WriteLine(n--);
+            //}
+
+
+            //int i = 1;
+            //if (i <= n)
+            //{
+            //    StampaNumeri(i + 1);
+            //    Console.WriteLine(n);
+            //}
+        }
+    }
+}
